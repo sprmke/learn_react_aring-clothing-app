dfcc4426a85dfec3f2fc2eaf7489e7dd501b53d5
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-10-19 08:34:56 +0800

    5.4 react router outlet

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Home from './pages/home/home.component';
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Home />}>
+        <Route path='shop' element={<p>Shop Page</p>} />
+      </Route>
     </Routes>
   );
 };
