7dc883438a1b5c30ba35c3bab76951cdb6ebec1d
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-10-19 08:44:03 +0800

    5.5 navigation bar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import { Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/home/home.component';
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>Navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
+const Shop = () => {
+  return <p>Shop Page</p>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />}>
-        <Route path='shop' element={<p>Shop Page</p>} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
   );
