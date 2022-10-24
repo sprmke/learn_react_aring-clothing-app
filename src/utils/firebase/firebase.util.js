fb2e16dc3eb5568116eed18e948fe04990c50689
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-10-25 04:51:53 +0800

    6.8 finish creating user documents with firestore

--- src/utils/firebase/firebase.util.js
+++ src/utils/firebase/firebase.util.js
@@ -49,5 +49,26 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   console.log('userSnapshot::', userSnapshot);
 
   // check if user document snapshot exists on the database
-  console.log('userSnapshot.exists()::', userSnapshot.exists());
+  // console.log('userSnapshot.exists()::', userSnapshot.exists());
+
+  // if user data does not exist
+  // create/set the document with the data from userAuth in my collection
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (err) {
+      console.log('Error creating the user', err.message);
+    }
+  }
+
+  // if user data exists
+  // return user doc reference
+  return userDocRef;
 };
