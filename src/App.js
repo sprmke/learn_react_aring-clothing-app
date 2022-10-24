878872355a9b3d1bd8486816c9e25a5464f9631a
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-10-25 02:55:25 +0800

    6.1 setting up firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import Home from './pages/home/home.component';
 import Navigation from './pages/navigation/navigation.component';
+import SignIn from './pages/sign-in/sign-in.component';
 
 const Shop = () => {
   return <p>Shop Page</p>;
@@ -13,6 +14,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
