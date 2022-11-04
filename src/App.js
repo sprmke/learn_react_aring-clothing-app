d53bf28c3a68a9db19c52366f06d60b2255d68ef
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-11-04 10:07:45 +0800

    9.10 creating checkout page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/home/home.component';
 import Navigation from './pages/navigation/navigation.component';
 import Shop from './pages/shop/shop.component';
 import Authentication from './pages/authentication/authentication.component';
+import Checkout from './pages/checkout/checkout.component';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
