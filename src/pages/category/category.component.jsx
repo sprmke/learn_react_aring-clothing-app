49f7c6cfddf80981694eeedef013be3e3adb7d0c
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-11-07 09:07:38 +0800

    10.8-10.9 Dynamic category page nested from shop route

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,38 @@
+import { useContext, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import ProductCard from '../../components/product-card/product-card.component';
+import { CategoriesContext } from '../../contexts/categories.context';
+
+import './category.styles.scss';
+
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState([]);
+
+  const categoryData = categoriesMap[category];
+
+  useEffect(() => {
+    if (categoryData) {
+      setProducts(categoryData);
+    }
+  }, [category, categoryData]);
+
+  let content = (
+    <p className='no-content'>No products found for '{category}' category</p>
+  );
+
+  if (products.length > 0) {
+    content = (
+      <div className='category-container'>
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    );
+  }
+
+  return content;
+};
+
+export default Category;
