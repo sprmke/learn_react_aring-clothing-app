19f9bfc98977f63d3c42dbe57e745cd06d0d0f3f
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-11-13 11:55:56 +0800

    14.9 transfer our business logic in our selectors

--- src/utils/firebase/firebase.util.js
+++ src/utils/firebase/firebase.util.js
@@ -101,14 +101,7 @@ export const getCategoriesAndDocuments = async () => {
   const querySnapshot = await getDocs(q);
 
   // restructure our categories to have cateogry: items format
-  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
-    const { title, items } = docSnapshot.data();
-    acc[title.toLowerCase()] = items;
-
-    return acc;
-  }, {});
-
-  return categoryMap;
+  return querySnapshot.docs.map((docSnapshot) => docSnapshot.data());
 };
 
 export const createUserDocumentFromAuth = async (
