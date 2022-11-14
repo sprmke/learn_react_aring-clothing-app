d2016fe2cc8905024d54bdffdfd1b8dd906d3a10
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-11-14 10:34:54 +0800

    14.11 create our own logger middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,24 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 
 const INITIAL_STATE = undefined;
 
-const middlewares = [logger];
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log('action.type::', action.type);
+  console.log('action.payload::', action.payload);
+  console.log('currentState::', store.getState());
+
+  next(action);
+
+  console.log('nextState::', store.getState());
+};
+
+const middlewares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
