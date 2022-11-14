42eea46a5b9620b063fb0699ce74175bec9e67b8
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-11-14 14:52:23 +0800

    15.2 enable logger middleware & redux devtools if not production

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,11 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
+// import { loggerMiddleware } from './middleware/logger';
 import { rootReducer } from './root-reducer';
 
 const INITIAL_STATE = undefined;
-
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log('action.type::', action.type);
-  console.log('action.payload::', action.payload);
-  console.log('currentState::', store.getState());
-
-  next(action);
-
-  console.log('nextState::', store.getState());
-};
 
 const persistConfig = {
   key: 'root',
@@ -28,9 +15,17 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [loggerMiddleware];
+const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(
+  Boolean
+);
 
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
 
 export const store = createStore(
   persistedReducer,
