59237e0f7173def96c07d7a05c4c6e65fe032651
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-11-14 11:28:22 +0800

    14. add reselect library and memoized categories selectors

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,9 +1,27 @@
-export const selectCategoriesMap = (state) => {
-  console.log('selector fired::');
-  return state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
+import { createSelector } from 'reselect';
 
-    return acc;
-  }, {});
+const selectCategoriesReducer = (state) => {
+  console.log('selector 1 fired::');
+  return state.categories;
 };
+
+const selectCategories = createSelector(
+  [selectCategoriesReducer],
+  (categoriesSlice) => {
+    console.log('selector 2 fired::');
+    return categoriesSlice.categories;
+  }
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => {
+    console.log('selector 3 fired::');
+    return categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+
+      return acc;
+    }, {});
+  }
+);
