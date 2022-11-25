14947cba210c5d29390174adeae39f783abfdc8a
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-11-25 08:01:54 +0800

    16.2 transfer async task inside redux thunk

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,8 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
-import { setCategories } from '../../store/categories/category.action';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.util';
+import { fetchCategoriesAsync } from '../../store/categories/category.action';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
@@ -10,12 +9,7 @@ const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments();
-      dispatch(setCategories(categoriesArray));
-    };
-
-    getCategoriesMap();
+    dispatch(fetchCategoriesAsync());
   }, [dispatch]);
 
   return (
--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,29 @@
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.util';
 import { createAction } from '../../utils/reducer/reducer.util';
 import { CATEGORIES_ACTION_TYPES } from './category.types';
 
-export const setCategories = (categoriesArray) => {
-  return createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+export const fetchCategoriesStart = () => {
+  return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+};
+
+export const fetchCategoriesSuccess = (categoriesArray) => {
+  return createAction(
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+    categoriesArray
+  );
+};
+
+export const fetchCategoriesFailed = (error) => {
+  return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+};
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+  dispatch(fetchCategoriesStart);
+
+  try {
+    const categoriesArray = await getCategoriesAndDocuments();
+    dispatch(fetchCategoriesSuccess(categoriesArray));
+  } catch (error) {
+    dispatch(fetchCategoriesFailed(error));
+  }
 };
